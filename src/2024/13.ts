3f696577d855653aadb4b8ff22c604c6b1a44b20
Thomas Lauerman <tom@lauerman.dev>
2024-12-15 19:55:21 -0800

    2024 day 13 part 2

--- src/2024/13.ts
+++ src/2024/13.ts
@@ -4,33 +4,20 @@ import fs from "node:fs";
 
 const input = fs.readFileSync(0, { encoding: "utf8" });
 
-let totalTokens = 0;
+let tokens = 0;
 for (const machine of input.split("\n\n")) {
-    const [ax, ay, bx, by, px, py] = machine.matchAll(/\d+/g)
+    let [ax, ay, bx, by, px, py] = machine.matchAll(/\d+/g)
         .map((m) => parseInt(m[0]));
 
-    for (let i = 100; i >= 0; i--) {
-        // assume pressing B is always optimal
-        // (it is in terms of pure cost, at least)
-        const x = bx * i;
-        const y = by * i;
+    px += 10_000_000_000_000;
+    py += 10_000_000_000_000;
 
-        if (x > px || y > py) {
-            continue;
-        }
-
-        if (x == px && y == py) {
-            // we didn't even need the A button!
-            totalTokens += i;
-            break;
-        }
-
-        const dx = px - x, dy = py - y;
-        if (dx % ax == 0 && dy % ay == 0 && dx / ax == dy / ay) {
-            totalTokens += i + 3 * (dx / ax);
-            break;
-        }
+    console.log(bx * py - px * by, bx * ay - ax * by);
+    const aPresses = (bx * py - px * by) / (bx * ay - ax * by);
+    const bPresses = (px - ax * aPresses) / bx;
+    if (aPresses % 1 == 0 && bPresses % 1 == 0) {
+        tokens += aPresses * 3 + bPresses;
     }
 }
 
-console.log(totalTokens);
+console.log(tokens);
